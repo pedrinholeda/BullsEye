e279a04ecbc3e90c3452e4cb61c7a7e5738854fb
Pedro Leda <pedro.henrique.leda@gmail.com>
2019-11-18 15:16:33 -0300

    implementando parte de radom

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var slider: UISlider!
+    var currentValue: Int = 50
+    var targetValue = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        currentValue = lroundf(slider.value)
+        targetValue = Int.random(in: 1...100)
        
     }
     
     @IBAction func showAlert() {
-      let alert = UIAlertController(title: "Hello, World",
-    message: "This is my first app!", preferredStyle: .alert)
+    let message = "The value of the slider is: \(currentValue)" +
+    "\nThe target value is: \(targetValue)"
+    let alert = UIAlertController(title: "Hello, World",
+    message: message, preferredStyle: .alert)
     let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
     alert.addAction(action)
       present(alert, animated: true, completion: nil)
@@ -25,7 +32,7 @@ class ViewController: UIViewController {
     
     
     @IBAction func sliderMoved(_ slider: UISlider){
-        print("O valor da sliderbar e : \(slider.value)")
+        currentValue = lroundf(slider.value)
     }
 
 }
